2dd2a6ef1d4cb04b46333efa5e2347e720a9254c
herereadthis <herereadthis@gmail.com>
2021-09-15 23:58:03 -0400

    more tables

--- tables.lua
+++ tables.lua
@@ -25,12 +25,14 @@ print(a[20])                --> great
 printTable(a)
 
 -- tables are anonymous
+print("\nanonymous demo")
 b = {}
 b["x"] = 10
-print(b["x"])                 --> 11
+print(b["x"])                 --> 10
 c = b
 c["x"] = 11
 print(b["x"])                 --> 11
+print(#b)                     --> 0
 
 -- tables will grow as needed
 d = {}
@@ -40,15 +42,43 @@ end
 print(d[0])                   --> 0.123
 print(d[500])                 --> 500.123
 print(d[1001])                --> nil
+print(#d)                     --> 1000
 
-
--- initialize a list
+print("\nList Example")
 days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
 print(days[0])                --> nil -- NOT ZERO INDEX
 print(days[4])                --> Wednesday
 
--- initialize a record
+print("\nRecord Example")
 myRecord = {x = 10, y = 20}
 myRecord.z = 30
 print(myRecord.x)             --> 10
 print(myRecord.z)             --> 30
+print(#myRecord)              --> 0
+
+print("\ntable traversal")
+
+function tableTraversePrint (t)
+  for k, v in pairs(t) do
+    print(k, v)
+  end
+end
+
+myTable = {10, "foo", x = 10, y = 20}
+tableTraversePrint(myTable)   --> 1 10, 2 foo, y 20, x 10
+
+-- Safe navigation
+print("\nSafe navigation")
+myNestedTable = {
+  foo = 123,
+  bar = {
+    x = 234,
+    baz = {
+      red = 5
+    }
+  }
+}
+print(myNestedTable.foo)      --> 123
+print(myNestedTable?.foo)
+
+
