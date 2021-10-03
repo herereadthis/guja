58c42779803015b752383092e77cbcf05ead8f27
herereadthis <herereadthis@gmail.com>
2021-10-02 23:58:04 -0400

    load

--- examples/compilation.lua
+++ examples/compilation.lua
@@ -1,5 +1,7 @@
 
+-- run a file
 function dofile (filename)
+  -- use assert to raise an error if loadfile fails
   local f = assert(loadfile(filename))
   return f()
 end
@@ -17,5 +19,16 @@ anyoneFile = getFilePath("hello-world.lua")
 
 print(anyoneFile)
 
-// run a file
+-- run a file
 dofile(anyoneFile)
+
+-- load is like loadfile, except it reads its chunk from a string or from a
+-- function, instead of a file.
+print("\nLoad example")
+local loadExample = load("myVar = 1 + 1")
+local functionExample = (i) i = i + 1 end -- the same as above, but cheaper
+myVar = 0 -- cannot be local!
+loadExample(); print(myVar)
+loadExample(); print(myVar)
+
+load(myVar)()
